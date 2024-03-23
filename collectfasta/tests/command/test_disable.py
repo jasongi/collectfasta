3c19b337e7decb03c072f2830fd41dff44ac5775
Jason Giancono <jasongiancono@gmail.com>
2024-03-24 01:43:57 +0800

    refactor tests to just use pytest

--- collectfasta/tests/command/test_disable.py
+++ collectfasta/tests/command/test_disable.py
@@ -1,18 +1,15 @@
-from unittest import TestCase
-from unittest import mock
-
 from django.test import override_settings as override_django_settings
+from pytest_mock import MockerFixture
 
+from collectfasta.tests.conftest import StrategyFixture
+from collectfasta.tests.conftest import live_test
 from collectfasta.tests.utils import clean_static_dir
 from collectfasta.tests.utils import create_static_file
-from collectfasta.tests.utils import live_test
-from collectfasta.tests.utils import make_test
 from collectfasta.tests.utils import override_setting
 
 from .utils import call_collectstatic
 
 
-@make_test
 @override_django_settings(
     STORAGES={
         "staticfiles": {
@@ -20,32 +17,33 @@ from .utils import call_collectstatic
         },
     },
 )
-def test_disable_collectfasta_with_default_storage(case: TestCase) -> None:
+def test_disable_collectfasta_with_default_storage() -> None:
     clean_static_dir()
     create_static_file()
-    case.assertIn("1 static file copied", call_collectstatic(disable_collectfasta=True))
+    assert "1 static file copied" in call_collectstatic(disable_collectfasta=True)
 
 
-@make_test
 @live_test
-def test_disable_collectfasta(case: TestCase) -> None:
+def test_disable_collectfasta(strategy: StrategyFixture) -> None:
     clean_static_dir()
     create_static_file()
-    case.assertIn(
-        "1 static file copied.", call_collectstatic(disable_collectfasta=True)
-    )
+    assert "1 static file copied" in call_collectstatic(disable_collectfasta=True)
 
 
 @override_setting("enabled", False)
-@mock.patch("collectfasta.management.commands.collectstatic.Command._load_strategy")
-def test_no_load_with_disable_setting(mocked_load_strategy: mock.MagicMock) -> None:
+def test_no_load_with_disable_setting(mocker: MockerFixture) -> None:
+    mocked_load_strategy = mocker.patch(
+        "collectfasta.management.commands.collectstatic.Command._load_strategy"
+    )
     clean_static_dir()
     call_collectstatic()
     mocked_load_strategy.assert_not_called()
 
 
-@mock.patch("collectfasta.management.commands.collectstatic.Command._load_strategy")
-def test_no_load_with_disable_flag(mocked_load_strategy: mock.MagicMock) -> None:
+def test_no_load_with_disable_flag(mocker: MockerFixture) -> None:
+    mocked_load_strategy = mocker.patch(
+        "collectfasta.management.commands.collectstatic.Command._load_strategy"
+    )
     clean_static_dir()
     call_collectstatic(disable_collectfasta=True)
     mocked_load_strategy.assert_not_called()
--- collectfasta/tests/strategies/test_caching_hash_strategy.py
+++ collectfasta/tests/strategies/test_caching_hash_strategy.py
@@ -1,12 +1,10 @@
 import string
-from unittest import TestCase
-from unittest import mock
 
 from django.core.files.storage import FileSystemStorage
+from pytest_mock import MockerFixture
 
 from collectfasta import settings
 from collectfasta.strategies.base import CachingHashStrategy
-from collectfasta.tests.utils import make_test
 
 hash_characters = string.ascii_letters + string.digits
 
@@ -19,55 +17,53 @@ class Strategy(CachingHashStrategy[FileSystemStorage]):
         pass
 
 
-@make_test
-def test_get_cache_key(case: TestCase) -> None:
+def test_get_cache_key() -> None:
     strategy = Strategy()
     cache_key = strategy.get_cache_key("/some/random/path")
     prefix_len = len(settings.cache_key_prefix)
-    case.assertTrue(cache_key.startswith(settings.cache_key_prefix))
-    case.assertEqual(32 + prefix_len, len(cache_key))
+    # case.assertTrue(cache_key.startswith(settings.cache_key_prefix))
+    assert cache_key.startswith(settings.cache_key_prefix)
+    assert len(cache_key) == 32 + prefix_len
     expected_chars = hash_characters + "_"
     for c in cache_key:
-        case.assertIn(c, expected_chars)
+        assert c in expected_chars
 
 
-@make_test
-def test_gets_and_invalidates_hash(case: TestCase) -> None:
+def test_gets_and_invalidates_hash(mocker: MockerFixture) -> None:
     strategy = Strategy()
     expected_hash = "hash"
-    with mock.patch.object(
-        strategy, "get_remote_file_hash", new=mock.MagicMock(return_value=expected_hash)
-    ) as mocked:
-        # empty cache
-        result_hash = strategy.get_cached_remote_file_hash("path", "prefixed_path")
-        case.assertEqual(result_hash, expected_hash)
-        mocked.assert_called_once_with("prefixed_path")
+    mocked = mocker.patch.object(
+        strategy,
+        "get_remote_file_hash",
+        new=mocker.MagicMock(return_value=expected_hash),
+    )
+    # empty cache
+    result_hash = strategy.get_cached_remote_file_hash("path", "prefixed_path")
+    assert result_hash == expected_hash
+    mocked.assert_called_once_with("prefixed_path")
 
-        # populated cache
-        mocked.reset_mock()
-        result_hash = strategy.get_cached_remote_file_hash("path", "prefixed_path")
-        case.assertEqual(result_hash, expected_hash)
-        mocked.assert_not_called()
+    # populated cache
+    mocked.reset_mock()
+    result_hash = strategy.get_cached_remote_file_hash("path", "prefixed_path")
+    assert result_hash == expected_hash
+    mocked.assert_not_called()
 
-        # test destroy_etag
-        mocked.reset_mock()
-        strategy.invalidate_cached_hash("path")
-        result_hash = strategy.get_cached_remote_file_hash("path", "prefixed_path")
-        case.assertEqual(result_hash, expected_hash)
-        mocked.assert_called_once_with("prefixed_path")
+    # test destroy_etag
+    mocked.reset_mock()
+    strategy.invalidate_cached_hash("path")
+    result_hash = strategy.get_cached_remote_file_hash("path", "prefixed_path")
+    assert result_hash == expected_hash
+    mocked.assert_called_once_with("prefixed_path")
 
 
-@make_test
-def test_post_copy_hook_primes_cache(case: TestCase) -> None:
+def test_post_copy_hook_primes_cache(mocker: MockerFixture) -> None:
     filename = "123abc"
     expected_hash = "abc123"
     strategy = Strategy()
 
-    with mock.patch.object(
+    mocker.patch.object(
         strategy, "get_local_file_hash", return_value=expected_hash, autospec=True
-    ):
-        strategy.post_copy_hook(filename, filename, strategy.remote_storage)
-
-    case.assertEqual(
-        expected_hash, strategy.get_cached_remote_file_hash(filename, filename)
     )
+    strategy.post_copy_hook(filename, filename, strategy.remote_storage)
+
+    assert strategy.get_cached_remote_file_hash(filename, filename) == expected_hash
